32f70f3b86880cc9ec8b12c5150606334b9f68e7
qtodd92 <120651462+qtodd92@users.noreply.github.com>
2024-05-16 05:57:52 -0400

    Initial commit

---
+++ src/test/java/com/quentbit/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quentbit.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
