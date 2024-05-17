bb68e293b3102ef33cbec27fce045c136fe8c231
qtodd92 <120651462+qtodd92@users.noreply.github.com>
2024-05-17 03:37:18 -0400

    adds database connection adds entity package and TaskList class adds JPA annotations

--- src/main/java/com/quentbit/todoapp/TodoappApplication.java
+++ src/main/java/com/quentbit/todoapp/TodoappApplication.java
@@ -1,13 +1,23 @@
 package com.quentbit.todoapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoappApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TodoappApplication.class, args);
+    }
 
+    @Bean
+    public CommandLineRunner commandLineRunner(String[] args) {
+
+        return runner -> {
+            System.out.println("Hello World");
+        };
+    }
 }
---
+++ src/main/java/com/quentbit/todoapp/entity/TaskList.java
@@ -0,0 +1,48 @@
+package com.quentbit.todoapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="task_list")
+public class TaskList {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="task")
+    private String task;
+
+    public TaskList() {
+    }
+
+    public TaskList(int id, String task) {
+        this.id = id;
+        this.task = task;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    @Override
+    public String toString() {
+        return "TaskList{" +
+                "id=" + id +
+                ", task='" + task + '\'' +
+                '}';
+    }
+}
